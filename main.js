801570e749986013044411c2b5674510967883ff
EvandersonOliveira <117873317+evandersonoliveira@users.noreply.github.com>
2023-10-26 10:58:32 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-document.querySelector('.tecla_pom') 
-document.querySelector('.tecla_Clap') 
-document.querySelector('.tecla_Tim') 
-document.querySelector('.tecla_Puff') 
-document.querySelector('.tecla_Splash')
-document.querySelector('.tecla_Toim') 
-document.querySelector('.tecla_Psh') 
-document.querySelector('.tecla_Tic') 
-document.querySelector('.tecla_Tom') 
+alert("Vá estudar nene")  
+
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play();}
+
+document.querySelector('.tecla_pom').onclick = tocaSompom;
+
+function tocaSomClap(){document.querySelector('#som_tecla_clap').play();} 
+
+document.querySelector('.tecla_Clap').onclick = tocaSomclap;
+
+function tocaSomTim(){document.querySelector('#som_tecla_tim').play();}
+
+document.querySelector('.tecla_Tim').onclick = tocaSomtim;
+
+function tocaSomPuff(){document.querySelector('#som_tecla_puff').play();}
+
+document.querySelector('.tecla_Puff').onclick = tocaSompuff;
+
+function tocaSomSplash(){document.querySelector('#som_tecla_splash').play();}
+
+document.querySelector('.tecla_Splash').onclick = tocaSomsplash;
+
+function tocaSomToim(){document.querySelector('#som_tecla_toim').play();}
+
+document.querySelector('.tecla_Toim').onclick = tocaSomtoim;
+
+function tocaSomPsh(){document.querySelector('#som_tecla_psh').play();}
+
+document.querySelector('.tecla_Psh').onclick = tocaSompsh;
+
+function tocaSomTic(){document.querySelector('#som_tecla_tic').play();}
+
+document.querySelector('.tecla_Tic').onclick = tocaSomtic;
+
+function tocaSomTom(){document.querySelector('#som_tecla_tom').play();}
+
+document.querySelector('.tecla_Tom').onclick = tocaSomTom;
+
+document.querySelectorAll('.tecla')
+
+NodeList(9) [button.tecla.tecla_pom, button.tecla.tecla_clap, button.tecla.tecla_tim, button.tecla.tecla_puff, button.tecla.tecla_splash, button.tecla.tecla_toim, button.tecla.tecla_psh, button.tecla.tecla_tic, button.tecla.tecla_tom]
